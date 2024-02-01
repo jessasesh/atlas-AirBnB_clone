e47d2c3d4fbfbfba2d30be7746d27dcb4e1d6780
lukeschula <schulaluke@gmail.com>
2024-02-01 17:31:22 +0000

    added shebang, and class

---
+++ models/base_model.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creating a class that defines all
+common attributes/methods for other classes"""
+
+
+class BaseModel:
+    """BaseModel class, used to meet the
+    docstring description above"""
+    def __init__(self, id,):
