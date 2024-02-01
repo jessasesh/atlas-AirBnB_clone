a7fc810c08218cff83646ecf3ee15ea127f4c5ea
lukeschula <schulaluke@gmail.com>
2024-02-01 18:04:53 +0000

    imported uuid

--- models/base_model.py
+++ models/base_model.py
@@ -2,8 +2,12 @@
 """creating a class that defines all
 common attributes/methods for other classes"""
 
+from uuid import uuid4
+
 
 class BaseModel:
     """BaseModel class, used to meet the
     docstring description above"""
-    def __init__(self, id,):
+    def __init__(self, id, created_at, updated_at):
+        """Public instance attributes"""
+
