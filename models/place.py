cf2df4a77f0bae2ded73b1d7020b87af54cd0ebb
jessasesh <jessica.dison@yahoo.com>
2024-02-05 05:34:43 +0000

    Added place attributes to correct file

--- models/place.py
+++ models/place.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
-"""module with place class"""
+"""
+This module defines the Place class.
+"""
+from models.base_model import BaseModel
+
+class Place(BaseModel):
+    """
+    Represents a place and its info.
+    """
+
+    city_id = ""
+    user_id = "" 
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids []
