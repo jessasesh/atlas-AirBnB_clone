73a17bddf615cf5f975856a55655375bbb00e343
lukeschula <schulaluke@gmail.com>
2024-02-05 17:30:29 +0000

    update for filestorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""
+"""import filestorage for
+storage of BaseModel"""
 
-"""
-#import file_storage.py
-#create the variable storage, an instance of FileStorage
-#call reload() method on this variable
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload
