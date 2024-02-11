5969614fec1738ba55f9e264d55c6a064b4b626f
lukeschula <schulaluke@gmail.com>
2024-02-11 20:43:36 +0000

    check

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,10 +1,1 @@
-#!/usr/bin/python3
-"""BaseModel unittest"""
 
-import unittest
-from models.base_model import BaseModel
-
-class TestBaseModel(unittest.TestCase):
-    """test BaseModel"""
-
-    def test_init(self):
--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -1,12 +1,2 @@
-#!/usr/bin/python3
-"""file storage unittests"""
-
-import unittest
-from models.engine.file_storage import FileStorage
 
 
-class TestFileStorage(unittest.TestCase):
-    """testing file storage"""
-
-    def test_all(self):
-        self.assertIsInstance(self.storage.all(), dict)
