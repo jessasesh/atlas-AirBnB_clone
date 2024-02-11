e8d2cefeefdfb919a0abad5655d3d721724013eb
lukeschula <schulaluke@gmail.com>
2024-02-11 19:53:48 +0000

    pycode fix

--- models/base_model.py
+++ models/base_model.py
@@ -14,11 +14,11 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Public instance attributes"""
         if kwargs:
-            for key, values in kwargs.items():
+            for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at":
-                        values = datetime.strptime(values, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, values)
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
